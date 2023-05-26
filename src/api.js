f1b606b2e083ebece7fcec2f2a5786232b939d70
Aditya <radaditya2311@gmail.com>
2023-05-26 11:10:03 +0530

    Initial Commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": 'c4ab093c8dmshc4e9e44337cae8fp18b0ecjsn29cb0cbd5233', // RapidAPI-Key
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"; // URL for GEO_API which returns longitude and latitude of the city.
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = '2c12a9450412fd798eb00c99be20b266';
